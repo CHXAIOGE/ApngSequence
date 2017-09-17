9e824c5c211ca5302deba96ede810c4687590f39
CHXAIOGE <18500738546@163.com>
2017-09-17 17:21:30 +0800

    add apng stream decode support

--- apng/src/main/java/com/xiaoge/graphics/apngsequence/utils/ApngHelper.java
+++ apng/src/main/java/com/xiaoge/graphics/apngsequence/utils/ApngHelper.java
@@ -15,7 +15,12 @@ import com.xiaoge.graphics.apngsequence.apngdrawable.ApngLoader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.xiaoge.graphics.apngsequence.apngdrawable.ApngImageUtils.getFileCachePath;
 
 /**
  * Created by zhanglei on 17-7-18.
@@ -28,6 +33,23 @@ public class ApngHelper {
         // load image
         String url = scheme.wrap(resource);
         ApngLoader.loadImageFile(url, view, listener);
+    }
+
+    public static void loadAnimatedPng(ImageView view, InputStream stream, @Nullable ApngImageLoadingListener listener) {
+        File destination;
+        try {
+            String md5 = getMD5(stream);
+            stream.reset();
+            destination = new File(getFileCachePath(ApngLoader.getAppContext(), md5));
+            if (!destination.exists()) {
+                ApngImageUtils.copyInputStreamToFile(stream, destination);
+            }
+            loadAnimatedPng(view, destination.getAbsolutePath(), listener);
+        } catch (Exception e) {
+            if(listener != null) {
+                listener.onLoadFailed(null, view);
+            }
+        }
     }
 
     public static boolean isAPNG(@NonNull File file) {
@@ -64,4 +86,21 @@ public class ApngHelper {
         }
     }
 
+    private static String getMD5(InputStream is) throws NoSuchAlgorithmException, IOException {
+        StringBuffer md5 = new StringBuffer();
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] dataBytes = new byte[1024];
+
+        int nread = 0;
+        while ((nread = is.read(dataBytes)) != -1) {
+            md.update(dataBytes, 0, nread);
+        }
+        byte[] mdbytes = md.digest();
+
+        // convert the byte to hex format
+        for (byte mdbyte : mdbytes) {
+            md5.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
+        }
+        return md5.toString();
+    }
 }
